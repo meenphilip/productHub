a12314f1783ec40207a3cac26d7299e5fcaf8dc6
Meendo <meenphilip@gmail.com>
2024-07-31 13:49:23 +0300

    Register product model in admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Product
+
 
 # Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["title", "price", "quantity", "producer", "slug"]
+    list_filter = ["title", "producer", "price"]
+    search_fields = ["title"]
+    prepopulated_fields = {"slug": ("title",)}
+    ordering = ["-title"]
