7aeaccb5d1400d363895dd77b9a9c2f8037a109f
Meendo <meenphilip@gmail.com>
2024-07-31 14:36:32 +0300

    Add product detail url

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.product_list, name="list"),
+    path("", views.product_list, name="product-list"),
+    path(
+        "product_detail/<int:id>/<slug:slug>/",
+        views.product_detail,
+        name="product-detail",
+    ),
 ]
