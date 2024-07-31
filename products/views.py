def2619ceafa30b2feee350bcb8f2a0112d97fcd
Meendo <meenphilip@gmail.com>
2024-07-31 13:50:07 +0300

    Create product list view

--- products/views.py
+++ products/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Product
+
 
 # Create your views here.
+def product_list(request):
+    # get all products
+    products = Product.objects.all()
+    return render(request, "products/list.html", {"products": products})
