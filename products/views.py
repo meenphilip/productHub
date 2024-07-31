4677dcef26ebc56db5b157ec0abd744e64ca05b9
Meendo <meenphilip@gmail.com>
2024-07-31 14:36:05 +0300

    Create product detail view

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
@@ -7,3 +7,10 @@ def product_list(request):
     # get all products
     products = Product.objects.all()
     return render(request, "products/list.html", {"products": products})
+
+
+# create product detail
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug)
+    context = {"product": product}
+    return render(request, "products/detail.html", context)
