911ab4f6e992aa27a67c1721fefd8233f7a64b35
Meendo <meenphilip@gmail.com>
2024-07-31 13:49:38 +0300

    Create product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    image = models.ImageField(upload_to="products/")
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField()
+    producer = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+
+    class Meta:
+        ordering = ["-title"]
+        indexes = [models.Index(fields=["title"])]
+
+    def __str__(self):
+        return f"{self.title}"
