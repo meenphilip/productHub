e038d8e3383c0df1167d0b0226a31bd8177e742e
Meendo <meenphilip@gmail.com>
2024-08-02 16:22:50 +0300

    Add widgets to forms fields

--- products/forms.py
+++ products/forms.py
@@ -6,5 +6,12 @@ from .models import Product
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        # fields = "__all__"
-        exclude = ["slug"]
+        fields = "__all__"
+        widgets = {
+            "title": forms.TextInput(attrs={"class": "form-control"}),
+            "image": forms.ClearableFileInput(attrs={"class": "form-control-file"}),
+            "price": forms.NumberInput(attrs={"class": "form-control"}),
+            "quantity": forms.NumberInput(attrs={"class": "form-control"}),
+            "producer": forms.TextInput(attrs={"class": "form-control"}),
+            "description": forms.Textarea(attrs={"class": "form-control"}),
+        }
