7210a171d8cb42fb874d0000972b298d7524d38b
Meendo <meenphilip@gmail.com>
2024-08-02 12:55:51 +0300

    Using canonical URLs for models

--- products/models.py
+++ products/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-from django.utils.text import slugify
+from django.urls import reverse
 
 
 # Create your models here.
 class Product(models.Model):
     title = models.CharField(max_length=200)
-    # slug = models.SlugField(max_length=200, unique_for_year="created_at")
     image = models.ImageField(upload_to="products/")
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField()
@@ -20,7 +19,5 @@ class Product(models.Model):
     def __str__(self):
         return f"{self.title}"
 
-    # def save(self, *args, **kwargs):
-    #     if not self.slug:
-    #         self.slug = slugify(self.title)
-    #     super().save(*args, **kwargs)
+    def get_absolute_url(self):
+        return reverse("product-detail", args=[self.id])
