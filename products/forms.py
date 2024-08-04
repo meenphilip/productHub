f154c00127772444821fd2fd94d1d4228d248828
Meendo <meenphilip@gmail.com>
2024-08-04 22:10:07 +0300

    Add placeholders attribute to form fields

--- products/forms.py
+++ products/forms.py
@@ -8,10 +8,35 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = "__all__"
         widgets = {
-            "title": forms.TextInput(attrs={"class": "form-control"}),
+            "title": forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Title",
+                }
+            ),
             "image": forms.ClearableFileInput(attrs={"class": "form-control-file"}),
-            "price": forms.NumberInput(attrs={"class": "form-control"}),
-            "quantity": forms.NumberInput(attrs={"class": "form-control"}),
-            "producer": forms.TextInput(attrs={"class": "form-control"}),
-            "description": forms.Textarea(attrs={"class": "form-control"}),
+            "price": forms.NumberInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Price",
+                }
+            ),
+            "quantity": forms.NumberInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Quantity",
+                }
+            ),
+            "producer": forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Producer",
+                }
+            ),
+            "description": forms.Textarea(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Write description",
+                }
+            ),
         }
